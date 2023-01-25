c9b10f7552d816be8284a9433f22ecbe61e252d4
miracle <mimiriath@gmail.com>
2023-01-25 06:01:37 +0100

    logstab adjusted to use components

--- v1/src/components/ProgramProviderComponents/shared/SingleProgramCandidateInfo/CandidateTabsDisplay/LogsTab/index.tsx
+++ v1/src/components/ProgramProviderComponents/shared/SingleProgramCandidateInfo/CandidateTabsDisplay/LogsTab/index.tsx
@@ -1,16 +1,7 @@
-import {
-	Typography,
-	Box,
-	FormControlLabel,
-	Checkbox,
-	Button,
-	CardContent,
-	Card,
-	Stack,
-	Avatar,
-} from "@mui/material";
+import { Typography, Box, FormControlLabel, Checkbox, Button, CardContent, Card, Stack, Avatar } from "@mui/material";
 import { Add, Close } from "@mui/icons-material";
 import React from "react";
+import InfoCard from "../../../InfoCard";
 
 interface DummyData {
 	autodisqualified: string[];
@@ -20,10 +11,7 @@ interface DummyData {
 
 const Index1 = (): JSX.Element => {
 	const [state, setState] = React.useState<DummyData>({
-		autodisqualified: [
-			"Have you graduated between 2020 and 2022",
-			"Are you willing to pay for the flight ticket?",
-		],
+		autodisqualified: ["Have you graduated between 2020 and 2022", "Are you willing to pay for the flight ticket?"],
 		disqualified: [
 			"Candidate was disqualified at Interview stage",
 			"We have checked the GPA and did not meet the requirement for the program",
@@ -39,102 +27,68 @@ const Index1 = (): JSX.Element => {
 	};
 
 	return (
-		<Box sx={{ mt: 1, ml: 1, p: 3 }}>
+		<Box sx={{ mt: 1, ml: 1, p: 3, "> *": { mb: 6 } }}>
 			{/* Auto disqualified*/}
-			<Card
-				sx={{
-					borderRadius: 3,
-					border: "1px solid #000",
-					p: 2,
-					mt: 5,
-					backgroundColor: "none",
-				}}>
-				<CardContent>
-					<Stack direction="row" spacing={2}>
-						<div>
-							<Avatar
-								sx={{ bgcolor: "#1D4ED8" }}
-								alt="Nathan Tom"
-								// src="/static/images/avatar/1.jpg"
-							>
-								NT
-							</Avatar>
-						</div>
-						<Box sx={{ display: "flex", flexDirection: "column" }}>
-							<Typography variant="caption" sx={{ fontSize: 12 }} gutterBottom>
-								Auto-disqualified
-							</Typography>
-							<Typography
-								variant="caption"
-								sx={{ fontSize: 14, mt: 1, fontWeight: 500 }}
-								gutterBottom>
-								Candidate said no to the following questions:
-							</Typography>
-							<Box
-								sx={{
-									mt: 1,
-								}}>
-								{state.autodisqualified.map((value, index) => (
-									<Stack key={index} direction="row" spacing={2}>
-										<Close />
-										<Typography sx={{ fontSize: 14 }}>{value}</Typography>
-									</Stack>
-								))}
-							</Box>
-							<Typography variant="caption" sx={{ fontSize: 12, mt: 3 }}>
-								{"Added by Kit"} | {"2023.01.15"}
-							</Typography>
-						</Box>
-					</Stack>
-				</CardContent>
-			</Card>
+
+			<InfoCard
+				title="Auto-disqualified"
+				image={
+					<Avatar
+						sx={{ bgcolor: "#1D4ED8" }}
+						alt="Nathan Tom"
+						// src="/static/images/avatar/1.jpg"
+					>
+						NT
+					</Avatar>
+				}
+				metadata={`Added by Kit | 2023.01.15`}>
+				<Box sx={{ display: "flex", flexDirection: "column" }}>
+					<Typography variant="caption" sx={{ fontSize: 14, mt: 1, fontWeight: 500 }} gutterBottom>
+						Candidate said no to the following questions:
+					</Typography>
+					<Box
+						sx={{
+							mt: 1,
+						}}>
+						{state.autodisqualified.map((value, index) => (
+							<Stack key={index} direction="row" spacing={2}>
+								<Close />
+								<Typography sx={{ fontSize: 14 }}>{value}</Typography>
+							</Stack>
+						))}
+					</Box>
+				</Box>
+			</InfoCard>
 
 			{/*Disqualified*/}
-			<Card
-				sx={{
-					borderRadius: 3,
-					border: "1px solid red",
-					p: 2,
-					mt: 5,
-					backgroundColor: "none",
-				}}>
-				<CardContent>
-					<Stack direction="row" spacing={2}>
-						<div>
-							<Avatar
-								sx={{ bgcolor: "#1D4ED8" }}
-								alt="Nathan Tom"
-								// src="/static/images/avatar/1.jpg"
-							>
-								NT
-							</Avatar>
-						</div>
-						<Box sx={{ display: "flex", flexDirection: "column" }}>
-							<Typography variant="caption" sx={{ fontSize: 12 }} gutterBottom>
-								Disqualified
-							</Typography>
+			<InfoCard
+				variant="error"
+				title="Disqualified"
+				metadata={`Added by Kit | 2023.01.15`}
+				image={
+					<Avatar
+						sx={{ bgcolor: "#1D4ED8" }}
+						alt="Nathan Tom"
+						// src="/static/images/avatar/1.jpg"
+					>
+						NT
+					</Avatar>
+				}>
+				{" "}
+				<Stack
+					direction="column"
+					spacing={2}
+					sx={{
+						mt: 1,
+					}}>
+					{state.disqualified.map((value, index) => (
+						<Typography key={index} sx={{ fontSize: 14, fontWeight: 500 }}>
+							{value}
+						</Typography>
+					))}
+				</Stack>
+			</InfoCard>
 
-							<Stack
-								direction="column"
-								spacing={2}
-								sx={{
-									mt: 1,
-								}}>
-								{state.disqualified.map((value, index) => (
-									<Typography
-										key={index}
-										sx={{ fontSize: 14, fontWeight: 500 }}>
-										{value}
-									</Typography>
-								))}
-							</Stack>
-							<Typography variant="caption" sx={{ fontSize: 12, mt: 3 }}>
-								{"Added by Kit"} | {"2023.01.15"}
-							</Typography>
-						</Box>
-					</Stack>
-				</CardContent>
-			</Card>
 			{/* <MessageArea /> */}
 		</Box>
 	);
