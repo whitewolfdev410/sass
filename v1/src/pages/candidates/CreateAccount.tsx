e4ea1d961dd0ffc4f4073768a5cbccb9e4c16c40
whitewolfdev410 <whitewolf96410@gmail.com>
2023-02-02 05:15:15 +0100

    phase1 candidate UI update

--- v1/src/pages/candidates/CreateAccount.tsx
+++ v1/src/pages/candidates/CreateAccount.tsx
@@ -1,95 +1,153 @@
-import React, {useState} from 'react';
-import {Box, Button, FormControl, Grid, Input, Stack, Typography} from "@mui/material";
-import {CandidateApplicationNav, ProgramSummaryCard} from "../../components/CandidatesComponents/shared";
-import {SidebarLayout} from "../../components/layout";
+import React, { useState } from "react";
+import {
+	Box,
+	Button,
+	FormControl,
+	Grid,
+	Input,
+	Stack,
+	Typography,
+} from "@mui/material";
+import {
+	CandidateApplicationNav,
+	ProgramSummaryCard,
+} from "../../components/CandidatesComponents/shared";
+import { SidebarLayout } from "../../components/layout";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import {candidateSignup} from "../../appStore/slices";
-import {useAppDispatch} from "../../appStore";
-import Hidden from '@mui/material/Hidden';
+import { candidateSignup } from "../../appStore/slices";
+import { useAppDispatch } from "../../appStore";
+import Hidden from "@mui/material/Hidden";
 
 const CreateAccount = () => {
+	const dispatch = useAppDispatch();
+	const applicationData = JSON.parse(
+		localStorage.getItem("candidateData") || "[]"
+	);
 
-    const dispatch = useAppDispatch();
-    const applicationData = JSON.parse(localStorage.getItem('candidateData') || '[]');
+	const [form, setForm] = useState({
+		userToken: "",
+		rePassword: "",
+	});
 
-    const [form, setForm] = useState({
-        userToken: "",
-        rePassword: ""
-    });
+	const handleChange = (event: any) => {
+		setForm({ ...form, [event.target.name]: event.target.value });
+	};
 
-    const handleChange = (event: any) => {
-        setForm({...form, [event.target.name]: event.target.value})
-    };
+	const onHandleSubmit = async () => {
+		applicationData.userToken = form.userToken;
+		const response = await dispatch(candidateSignup(applicationData));
+		console.log("response", response);
+	};
 
-    const onHandleSubmit = async () => {
-        applicationData.userToken = form.userToken;
-        const response = await dispatch(candidateSignup(applicationData));
-        console.log("response",response)
-    };
+	return (
+		<SidebarLayout screen="/candidate/apply/program-application">
+			<Box
+				sx={{ mt: "-50px" }}
+				className="header">
+				<CandidateApplicationNav completed={2} />
+			</Box>
+			<Stack
+				direction="row"
+				flexWrap="wrap"
+				gap={5}
+				mt="144px"
+				ml="86px"
+				mr="80px"
+				className="content-wrapper"
+				justifyContent="space-between">
+				<Box
+					className="auth-candidate"
+					sx={{ mt: "10px" }}>
+					<Typography
+						variant="h1"
+						component="h1"
+						sx={{ mb: 3 }}>
+						Final step- create your <br /> account
+					</Typography>
+					<Typography
+						fontSize={14}
+						sx={{ mb: 3 }}>
+						By creating the account, I have agreed to the Terms and Conditions
+						and Privacy Policy.
+					</Typography>
+					<form>
+						<Grid
+							container
+							columnSpacing={4}>
+							<Grid
+								item
+								md={6}>
+								<FormControl
+									variant="standard"
+									sx={{ my: 3 }}>
+									<label>Password*</label>
+									<Input
+										onChange={handleChange}
+										value={form.userToken}
+										name="userToken"
+										required
+									/>
+								</FormControl>
+							</Grid>
+							<Grid
+								item
+								md={6}>
+								<FormControl
+									variant="standard"
+									sx={{ my: 3 }}>
+									<label>Re enter Password*</label>
+									<Input
+										onChange={handleChange}
+										value={form.rePassword}
+										name="rePassword"
+										required
+									/>
+								</FormControl>
+							</Grid>
+						</Grid>
 
-    return (
-        <SidebarLayout screen="/candidate/apply/program-application">
-            <Box sx={{mt: "-50px"}} className="header">
-                <CandidateApplicationNav completed={2}/>
-            </Box>
-            <Stack direction="row" flexWrap="wrap" gap={5} mt={20} className="content-wrapper" justifyContent="space-between">
-                <Box className="auth-candidate">
-                    <Typography variant="h1" component="h1" sx={{ mb: 3 }}>
-                        Final step- create your  <br /> account
-                    </Typography>
-                    <Typography fontSize={14} sx={{ mb: 3 }}>
-                        By creating the account, I have agreed to the Terms and Conditions and Privacy Policy.
-                    </Typography>
-                    <form>
-                        <Grid container columnSpacing={4}>
-                            <Grid item md={6}>
-                                <FormControl variant="standard" sx={{ my: 3 }}>
-                                    <label>Password*</label>
-                                    <Input onChange={handleChange} value={form.userToken} name="userToken" required />
-                                </FormControl>
-                            </Grid>
-                            <Grid item md={6}>
-                                <FormControl variant="standard" sx={{ my: 3 }}>
-                                    <label>Re enter Password*</label>
-                                    <Input onChange={handleChange} value={form.rePassword} name="rePassword" required />
-                                </FormControl>
-                            </Grid>
-                        </Grid>
-
-                        <Button variant="contained" size="large" fullWidth sx={{ my: 3, py: 3 }} onClick={()=>onHandleSubmit()}>
-                            Create an account
-                            <ArrowForwardIosIcon sx={{ ml: 1 }} />
-                        </Button>
-                    </form>
-                </Box>
-                <Hidden smDown>
-                    <Box>
-                        <ProgramSummaryCard image data={data}/>
-                    </Box>
-                </Hidden>
-            </Stack>
-        </SidebarLayout>
-    )
+						<Button
+							variant="contained"
+							size="large"
+							fullWidth
+							sx={{ my: 3, py: 3 }}
+							onClick={() => onHandleSubmit()}>
+							Create an account
+							<ArrowForwardIosIcon sx={{ ml: 1 }} />
+						</Button>
+					</form>
+				</Box>
+				<Hidden smDown>
+					<Box>
+						<ProgramSummaryCard
+							image
+							data={data}
+						/>
+					</Box>
+				</Hidden>
+			</Stack>
+		</SidebarLayout>
+	);
 };
 
 export default CreateAccount;
 
 let data = {
-    id: "string",
-    programID: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    title: "string",
-    description: "string",
-    summary: "string",
-    keySkills: "string",
-    programBenefits: "string",
-    applicationCriteria: "string",
-    programType: 0,
-    minQualification: 0,
-    startDate: "2023-01-22T00:20:41.450Z",
-    appOpenDate: "2023-01-22T00:20:41.450Z",
-    appCloseDate: "2023-01-22T00:20:41.450Z",
-    duration: "string",
-    locationID: 0,
-    maxAppCount: 0,
-    createdOn: "2023-01-22T00:20:41.450Z",
+	id: "string",
+	programID: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+	title: "string",
+	description: "string",
+	summary: "string",
+	keySkills: "string",
+	programBenefits: "string",
+	applicationCriteria: "string",
+	programType: 0,
+	minQualification: 0,
+	startDate: "2023-01-22T00:20:41.450Z",
+	appOpenDate: "2023-01-22T00:20:41.450Z",
+	appCloseDate: "2023-01-22T00:20:41.450Z",
+	duration: "string",
+	locationID: 0,
+	maxAppCount: 0,
+	createdOn: "2023-01-22T00:20:41.450Z",
 };
--- v1/src/pages/candidates/dashboard/index.tsx
+++ v1/src/pages/candidates/dashboard/index.tsx
@@ -10,16 +10,36 @@ import { Box, Stack, Divider, Typography } from "@mui/material";
 const Dashboard = () => {
 	return (
 		<SidebarLayout>
-			<Box className="content-wrapper" ml="initial">
-				<Stack direction="row" gap={2} mt={3}>
-					<ProgramInfoCard data={data[0]} image />
-					<ProgramInfoCard data={data[1]} image />
-					<ProgramInfoCard data={data[1]} image />
+			<Box
+				className="content-wrapper"
+				ml="initial">
+				<Stack
+					direction="row"
+					gap={2}
+					mt={3}>
+					<ProgramInfoCard
+						data={data[0]}
+						image
+					/>
+					<ProgramInfoCard
+						data={data[1]}
+						image
+					/>
+					<ProgramInfoCard
+						data={data[1]}
+						image
+					/>
 				</Stack>
 				<Divider sx={{ mt: 10, mb: 5, width: "70%" }} />
 				<Typography variant="h2">Past programmes</Typography>
-				<Stack direction="row" gap={2} mt={5}>
-					<ProgramInfoCard data={data[2]} image />
+				<Stack
+					direction="row"
+					gap={2}
+					mt={5}>
+					<ProgramInfoCard
+						data={data[2]}
+						image
+					/>
 				</Stack>
 			</Box>
 		</SidebarLayout>
@@ -31,8 +51,8 @@ export default Dashboard;
 let data = [
 	{
 		title: "Strategic gears, Internship programme",
-		appOpenDate: "2023-01-29T12:58:20.401Z",
-		appCloseDate: "2023-01-29T12:58:20.401Z",
+		appOpenDate: "2023-02-29T12:58:20.401Z",
+		appCloseDate: "2023-01-21T12:58:20.401Z",
 	},
 	{
 		title: "MiSK Startup School, Masterclasses",
